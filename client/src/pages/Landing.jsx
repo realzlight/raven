1962c331343fa07066b39e1d81091ffbcbe5e783
realzlight <legitzlight@gmail.com>
2026-07-19 10:48:34 +0530

    update

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -6,51 +6,7 @@ import CardNav from '../components/CardNav.jsx'
 export default function Landing() {
   const navigate = useNavigate()
 
-  const items = [
-    {
-      label: "About",
-      bgColor: "#1B1722",
-      textColor: "#fff",
-      links: [
-        { label: "Company", ariaLabel: "About Company" },
-        { label: "Careers", ariaLabel: "About Careers" }
-      ]
-    },
-    {
-      label: "Projects",
-      bgColor: "#2F293A",
-      textColor: "#fff",
-      links: [
-        { label: "Featured", ariaLabel: "Featured Projects" },
-        { label: "Case Studies", ariaLabel: "Project Case Studies" }
-      ]
-    },
-    {
-      label: "Contact",
-      bgColor: "#2F293A",
-      textColor: "#fff",
-      links: [
-        { label: "Email", ariaLabel: "Email us" },
-        { label: "Twitter", ariaLabel: "Twitter" },
-        { label: "LinkedIn", ariaLabel: "LinkedIn" }
-      ]
-    }
-  ]
-
   return (
-    <CardNav
-      logoText="RAVEN"
-      items={items}
-      baseColor="#fff"
-      menuColor="#000"
-      buttonBgColor="#111"
-      buttonTextColor="#fff"
-      ease="power3.out"
-    />
-    
-    
-    
+    <CardNav logoText="RAVEN" />
   )
-  
-  
 }
